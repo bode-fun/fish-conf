0b51fc37aa52b8a775d271678707d779bdcdcb78
Jan Fooken <git@bode.fun>
2023-03-27 12:10:27 +0200

    fix linux case for os_discovery

--- functions/bode_os_discovery.fish
+++ functions/bode_os_discovery.fish
@@ -7,7 +7,7 @@
 function bode_os_discovery --description "Detect and print the operating system name"
     set os_name (uname -s)
     set os_type $os_name
-    if test "$os_name" = Linux && test -n WSL_DISTRO_NAME
+    if test "$os_name" = Linux && test -n $WSL_DISTRO_NAME
         set os_type WSL
     end
     echo $os_type
