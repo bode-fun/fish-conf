c2133633731d9a7b09946d75223a56e495e0f9da
Jan Fooken <git@bode.fun>
2023-05-08 14:46:58 +0200

    Fix shell integration of homebrew

--- conf.d/00-brew.fish
+++ conf.d/00-brew.fish
@@ -2,9 +2,9 @@
 set os_name (bode_os_discovery)
 
 if test $os_name = Darwin
-    /opt/homebrew/bin/brew shellenv | source
+    /opt/homebrew/bin/brew shellenv fish | source
 else if test $os_name = WSL || test $os_name = Linux
-    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
 else
     echo "Unsupported OS: $os_name"
     return 1
